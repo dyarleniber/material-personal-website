b1db0c3c90b7ddb35c08e8149ef5be7589e48297
Dyarlen Iber <dyarlen1@gmail.com>
2020-06-14 02:08:54 +0100

    Web font loader

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 
+import "../src/config/fonts";
 import "../src/config/icons";
 import theme from "../src/config/theme";
 import MenuBar from "./components/MenuBar";
