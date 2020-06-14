b907c2f45d28d92749e687a20a9a9059676c4826
Dyarlen Iber <dyarlen1@gmail.com>
2020-06-14 03:19:32 +0100

    HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 
